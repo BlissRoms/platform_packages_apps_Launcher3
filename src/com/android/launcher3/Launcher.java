e0ad59a42f89510e341b02dc23efa18dd3577419
ElDainosor <eldainosor@gmail.com>
2019-06-19 20:30:36 -0300

    Quickspace: refresh the views more frequently
    based on
    https://github.com/amirzaidi/Shade/commit/1bea24de578805bf124ab853e34a827536c14015
    
    Change-Id: I9b275d25f2025b7cbbf3d9809359dcbec3e31f1b
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -1030,6 +1030,10 @@ public class Launcher extends BaseDraggingActivity implements LauncherExterns,
             mClient.onResume();
         }
 
+        if (mQuickSpace != null) {
+            mQuickSpace.onResume();
+        }
+
         if (mLauncherCallbacks != null) {
             mLauncherCallbacks.onResume();
         }
