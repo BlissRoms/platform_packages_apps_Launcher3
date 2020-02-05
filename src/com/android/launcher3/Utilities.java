27a7f9c5b83f4163780bec8e291ff48db3138cb6
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-02-05 22:05:00 +0200

    Make double tap to sleep gesture optional
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: I159a26aa75e8f6161da4772628235bc726354a73

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -115,6 +115,8 @@ public final class Utilities {
      */
     public static final int EDGE_NAV_BAR = 1 << 8;
 
+    public static final String SLEEP_GESTURE = "pref_sleep_gesture";
+
     /**
      * Indicates if the device has a debug build. Should only be used to store additional info or
      * add extra logging and not for changing the app behavior.
@@ -612,6 +614,10 @@ public final class Utilities {
         public int getIntrinsicWidth() {
             return mSize;
         }
+    }
+
+    public static boolean useSleepGesture(Context context) {
+        return getPrefs(context).getBoolean(SLEEP_GESTURE, true);
     }
 
     public static void restart(final Context context) {
