6af219afe29ca346695256ed12cfe8b43bf1d6ae
Pranav Vashi <neobuddy89@gmail.com>
2020-03-09 01:01:17 +0530

    Launcher: Call mStateManager directly
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -493,7 +493,7 @@ public class Launcher extends BaseDraggingActivity implements LauncherExterns,
                     ? RotationMode.NORMAL : UiFactory.getRotationMode(mDeviceProfile);
         }
         getRootView().dispatchInsets();
-        getStateManager().reapplyState(cancelCurrentAnimation);
+        mStateManager.reapplyState(cancelCurrentAnimation);
     }
 
     @Override
@@ -774,7 +774,7 @@ public class Launcher extends BaseDraggingActivity implements LauncherExterns,
                 final Runnable onComplete = new Runnable() {
                     @Override
                     public void run() {
-                        getStateManager().goToState(NORMAL);
+                        mStateManager.goToState(NORMAL);
                     }
                 };
 
@@ -933,7 +933,7 @@ public class Launcher extends BaseDraggingActivity implements LauncherExterns,
         mAppWidgetHost.setListenIfResumed(false);
 
         NotificationListener.removeNotificationsChangedListener();
-        getStateManager().moveToRestState();
+        mStateManager.moveToRestState();
 
         UiFactory.onLauncherStateOrResumeChanged(this);
 
@@ -2646,7 +2646,7 @@ public class Launcher extends BaseDraggingActivity implements LauncherExterns,
             switch (keyCode) {
                 case KeyEvent.KEYCODE_A:
                     if (isInState(NORMAL)) {
-                        getStateManager().goToState(ALL_APPS);
+                        mStateManager.goToState(ALL_APPS);
                         return true;
                     }
                     break;
@@ -2708,7 +2708,7 @@ public class Launcher extends BaseDraggingActivity implements LauncherExterns,
     @Override
     public void returnToHomescreen() {
         super.returnToHomescreen();
-        getStateManager().goToState(LauncherState.NORMAL);
+        mStateManager.goToState(LauncherState.NORMAL);
     }
 
     /**
