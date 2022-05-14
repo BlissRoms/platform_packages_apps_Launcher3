656e66a13b69b3542ce164221f8f42959dc840c6
Ratoriku <a1063021545@gmail.com>
2022-05-14 20:07:48 +0800

    Launcher3: Add toggle to use dark status bar on home screen
    * Since top shadow is allowed to be disabled, use dark
      status bar is a good idea if a light wallpaper is in
      current.
    
    * Inspired from lawnchair:
      https://github.com/LawnchairLauncher/lawnchair/commit/fd2de2173fbbb8b2f349a51352791432da429d90#
    
    Change-Id: I682fc4ba36415564086927ae1264e33c012fd28f
    Co-authored-by: Suphon Thanakornpakapong <paphonb@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -298,7 +298,9 @@ import java.util.stream.Stream;
  */
 public class Launcher extends StatefulActivity<LauncherState>
         implements Callbacks, InvariantDeviceProfile.OnIDPChangeListener,
-        PluginListener<LauncherOverlayPlugin> {
+        PluginListener<LauncherOverlayPlugin>,
+        SharedPreferences.OnSharedPreferenceChangeListener{
+
     public static final String TAG = "Launcher";
 
     public static final ContextTracker.ActivityTracker<Launcher> ACTIVITY_TRACKER =
@@ -599,7 +601,8 @@ public class Launcher extends StatefulActivity<LauncherState>
         // Listen for screen turning off
         ScreenOnTracker.INSTANCE.get(this).addListener(mScreenOnListener);
         getSystemUiController().updateUiState(SystemUiController.UI_STATE_BASE_WINDOW,
-                Themes.getAttrBoolean(this, R.attr.isWorkspaceDarkText));
+                Themes.getAttrBoolean(this, R.attr.isWorkspaceDarkText)
+                || LauncherPrefs.DARK_STATUS_BAR.get(this));
 
         mOverlayManager = getDefaultOverlay();
         PluginManagerWrapper.INSTANCE.get(this)
@@ -618,6 +621,15 @@ public class Launcher extends StatefulActivity<LauncherState>
                     RuleController.parseRules(this, R.xml.split_configuration));
         }
         TestEventEmitter.sendEvent(TestEvent.LAUNCHER_ON_CREATE);
+
+        mSharedPrefs.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (LauncherPrefs.DARK_STATUS_BAR.getSharedPrefKey().equals(key)) {
+            recreate();
+        }
     }
 
     protected ModelCallbacks createModelCallbacks() {
@@ -1808,6 +1820,8 @@ public class Launcher extends StatefulActivity<LauncherState>
     @Override
     public void onDestroy() {
         super.onDestroy();
+        mSharedPrefs.unregisterOnSharedPreferenceChangeListener(this);
+
         ACTIVITY_TRACKER.onContextDestroyed(this);
 
         SettingsCache.INSTANCE.get(this).unregister(TOUCHPAD_NATURAL_SCROLLING,
