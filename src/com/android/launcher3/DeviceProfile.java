bc7c68b9a7e42358855e93bcfedd1d35c845611a
Pranav Vashi <neobuddy89@gmail.com>
2022-06-05 12:55:44 +0530

    Launcher3: Add pref to set app drawer row height
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -159,6 +159,7 @@ public class DeviceProfile {
     public int allAppsCloseDuration;
     public int allAppsLeftRightMargin;
     public final int numShownAllAppsColumns;
+    private float allAppsCellHeightMultiplier;
 
     private final OverviewProfile overviewProfile;
 
@@ -312,6 +313,9 @@ public class DeviceProfile {
                 mTypeIndex,
                 inv
         );
+
+        allAppsCellHeightMultiplier =
+                    (float) LauncherPrefs.ROW_HEIGHT.get(context) / 100F;
 
         setupAllAppsStyle(context);
 
@@ -631,7 +635,7 @@ public class DeviceProfile {
      */
     public int getMaxAllAppsRowCount() {
         return (int) (Math.ceil((mDeviceProperties.getAvailableHeightPx() - allAppsPadding.top)
-                / (float) getAllAppsProfile().getCellHeightPx()));
+                / ((float) getAllAppsProfile().getCellHeightPx() * allAppsCellHeightMultiplier)));
     }
 
     /**
@@ -866,8 +870,8 @@ public class DeviceProfile {
                 && !(mIsResponsiveGrid && getAllAppsProfile().getMaxAllAppsTextLineCount() == 2)) {
             // Add extra textHeight to the existing allAppsCellHeight.
             mAllAppsProfile = getAllAppsProfile().copyWithCellHeightPx(
-                    getAllAppsProfile().getCellHeightPx() + Utilities.calculateTextHeight(
-                            getAllAppsProfile().getIconTextSizePx())
+                    (int) ((float) getAllAppsProfile().getCellHeightPx() * allAppsCellHeightMultiplier)
+                    + Utilities.calculateTextHeight( getAllAppsProfile().getIconTextSizePx())
             );
         }
 
