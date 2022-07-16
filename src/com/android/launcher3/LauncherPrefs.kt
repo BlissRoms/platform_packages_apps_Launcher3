c17d6c2296f6e819860944c3404b2eae7cc55114
minaripenguin <alexfinhart@gmail.com>
2022-07-16 08:35:11 +0800

    Launcher3: Add an option to customize blur radius
    * Set max blur radius to 175px
    
    @neobuddy89:
    * Add some clean up. Updated for A13 QPR1
    * Updated for A16
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- src/com/android/launcher3/LauncherPrefs.kt
+++ src/com/android/launcher3/LauncherPrefs.kt
@@ -246,6 +246,7 @@ constructor(@ApplicationContext private val encryptedContext: Context) {
         @JvmField val ALLAPPS_THEMED_ICONS = backedUpItem("pref_allapps_themed_icons", false)
         @JvmField val ALLOW_WALLPAPER_ZOOMING = backedUpItem("pref_allow_wallpaper_zooming", true)
         @JvmField val APP_DRAWER_OPACITY = backedUpItem("pref_app_drawer_opacity", 80)
+        @JvmField val BLUR_DEPTH = backedUpItem("pref_blur_depth", 23)
         @JvmField val DARK_STATUS_BAR = backedUpItem("pref_dark_status_bar", false)
         @JvmField val DOCK_MUSIC_SEARCH = backedUpItem("pref_dock_music_search", false)
         @JvmField val DOCK_SEARCH = backedUpItem("pref_dock_search", true)
