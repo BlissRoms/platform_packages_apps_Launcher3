95cc26401f187079b0500f7617d0268836076eed
Pranav Vashi <neobuddy89@gmail.com>
2022-09-09 01:01:31 +0530

    Launcher3: Update pause apps feature dialog
    * Use outline icon for hourglass.
    * Do not show icon with dialogs - looks monstrous and out of theme.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- quickstep/src/com/android/launcher3/model/WellbeingModel.java
+++ quickstep/src/com/android/launcher3/model/WellbeingModel.java
@@ -368,7 +368,7 @@ public final class WellbeingModel implements SafeCloseable {
     public static class PauseApps<T extends ActivityContext> extends SystemShortcut<T> {
 
         public PauseApps(T target, ItemInfo itemInfo, View originalView) {
-            super(R.drawable.ic_hourglass_top, R.string.paused_apps_drop_target_label, target,
+            super(R.drawable.ic_hourglass, R.string.paused_apps_drop_target_label, target,
                     itemInfo, originalView);
         }
 
@@ -387,7 +387,6 @@ public final class WellbeingModel implements SafeCloseable {
 
             }
             new AlertDialog.Builder(context)
-                    .setIcon(R.drawable.ic_hourglass_top)
                     .setTitle(context.getString(R.string.pause_apps_dialog_title, appLabel))
                     .setMessage(context.getString(R.string.pause_apps_dialog_message, appLabel))
                     .setNegativeButton(android.R.string.cancel, null)
