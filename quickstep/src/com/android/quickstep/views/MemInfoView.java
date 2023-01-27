397e7dda6f2c2ed9da3cf6c4bcd5d562f89d4938
minaripenguin <minaripenguin@users.noreply.github.com>
2023-01-27 17:39:14 +0800

    Launcher3: Open running services activity when clicking meminfo view
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- quickstep/src/com/android/quickstep/views/MemInfoView.java
+++ quickstep/src/com/android/quickstep/views/MemInfoView.java
@@ -18,6 +18,7 @@ package com.android.quickstep.views;
 
 import android.app.ActivityManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.AttributeSet;
@@ -80,6 +81,7 @@ public class MemInfoView extends TextView {
         mWorker = new MemInfoWorker();
 
         mMemInfoText = context.getResources().getString(R.string.meminfo_text);
+        setListener(context);
     }
 
     /* Hijack this method to detect visibility rather than
@@ -136,6 +138,15 @@ public class MemInfoView extends TextView {
         String text = String.format(mMemInfoText,
             unitConvert(availMemMiB, false), unitConvert(totalMemMiB, true));
         setText(text);
+    }
+
+    public void setListener(Context context) {
+        setOnClickListener(view -> {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            intent.setClassName("com.android.settings", "com.android.settings.Settings$DevRunningServicesActivity");
+            context.startActivity(intent);
+        });
     }
 
     private class MemInfoWorker implements Runnable {
