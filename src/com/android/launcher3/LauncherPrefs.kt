03be5c1e944dbed4905006c3fa0d08fa7343d7d1
LordShenron <shen.priyanshu@gmail.com>
2023-04-02 20:16:50 +0800

    Launcher3: Vibrate on double tap to sleep
    [HELLBOY017: Make it optional]
    
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- src/com/android/launcher3/LauncherPrefs.kt
+++ src/com/android/launcher3/LauncherPrefs.kt
@@ -255,6 +255,7 @@ constructor(@ApplicationContext private val encryptedContext: Context) {
         @JvmField val SHOW_DRAWER_LABELS = backedUpItem("pref_drawer_show_labels", true)
         @JvmField val NOTIFICATION_BADGE_COUNTS = backedUpItem("pref_notification_badge_counts", false)
         @JvmField val SLEEP_GESTURE = backedUpItem("pref_sleep_gesture", false)
+        @JvmField val SLEEP_GESTURE_HAPTIC = backedUpItem("pref_sleep_gesture_haptic", true)
         @JvmField
         val PROMISE_ICON_IDS = nonRestorableItem(InstallSessionHelper.PROMISE_ICON_IDS, "")
         @JvmField val WORK_EDU_STEP = backedUpItem("showed_work_profile_edu", 0)
