800aeadeb1ac2b8bb73220e6b1f570162ccc269e
hmtheboy154 <buingoc67@gmail.com>
2023-07-14 05:16:14 -0400

    Temporary hardcode Split icon
    Use ic_split_horizontal until we can be able to make it change dynamically
    on OverviewActionsView.
    
    Change-Id: Ifdd73c5eab1878c08a5a3de7ab8955ef0c9fe2e9
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- quickstep/src/com/android/quickstep/views/OverviewActionsView.java
+++ quickstep/src/com/android/quickstep/views/OverviewActionsView.java
@@ -386,9 +386,6 @@ public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayo
 
         requestLayout();
 
-        mSplitButton.setCompoundDrawablesRelativeWithIntrinsicBounds(
-                (dp.isLandscape ? R.drawable.ic_split_horizontal : R.drawable.ic_split_vertical),
-                0, 0, 0);
     }
 
     /**
