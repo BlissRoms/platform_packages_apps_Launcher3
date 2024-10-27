46fc72c342a58ce2124439af776b40ade27bed40
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-10-27 07:41:48 +0530

    Launcher3: Improve meminfo view height and margins
    - Take account of meminfo view height in overview to avoid overlapping
      in landscape mode.
    - Borrow the overview actions' bottom margin instead of hardcoding it.
    
    Change-Id: I4e5efb63ebfa0cb35bcaf2fb1008a6655e567de6
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- src/com/android/launcher3/settings/SettingsRecents.java
+++ src/com/android/launcher3/settings/SettingsRecents.java
@@ -16,11 +16,14 @@
 
 package com.android.launcher3.settings;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.preference.PreferenceFragmentCompat;
 
+import com.android.launcher3.LauncherAppState;
 import com.android.launcher3.LauncherFiles;
+import com.android.launcher3.LauncherPrefs;
 import com.android.launcher3.R;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
@@ -29,7 +32,8 @@ import com.android.settingslib.widget.SettingsBasePreferenceFragment;
 /**
  * Recents settings activity for Launcher.
  */
-public class SettingsRecents extends CollapsingToolbarBaseActivity {
+public class SettingsRecents extends CollapsingToolbarBaseActivity
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +44,20 @@ public class SettingsRecents extends CollapsingToolbarBaseActivity {
                     .beginTransaction()
                     .replace(com.android.settingslib.collapsingtoolbar.R.id.content_frame, new RecentsSettingsFragment())
                     .commit();
+        }
+        LauncherPrefs.getPrefs(this).registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        LauncherPrefs.getPrefs(this).unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (LauncherPrefs.RECENTS_MEMINFO.getSharedPrefKey().equals(key)) {
+            LauncherAppState.INSTANCE.executeIfCreated(app -> app.setNeedsRestart());
         }
     }
 
