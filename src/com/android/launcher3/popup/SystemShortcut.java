RisingOS-staging/android_packages_apps_Launcher3

Launcher3: Add force close system shortcut
* this is useful for some apps like vpn, termux or apps that doesn't show on recents but runs on background

Change-Id: I0754aceae796eb080228c6a584054bad64f9a162
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

src/com/android/launcher3/popup/SystemShortcut.java
@@ -13,6 +13,7 @@ import static android.app.ActivityOptions.MODE_BACKGROUND_ACTIVITY_START_ALLOWED
 import android.app.Activity;
 import android.app.ActivityManagerNative;
 import android.app.ActivityOptions;
+import android.app.IActivityManager;
 import android.app.AlertDialog;
 import android.app.AppGlobals;
 import android.content.ComponentName;
@@ -434,6 +435,34 @@ public abstract class SystemShortcut<T extends Context & ActivityContext> extend
                     MODE_BACKGROUND_ACTIVITY_START_ALLOWED);
             activityOptions.setPendingIntentBackgroundActivityLaunchAllowedByPermission(true);
             return activityOptions;
+        }
+    }
+
+    public static final Factory<BaseDraggingActivity> KILL_APP = (activity, itemInfo, originalView) -> {
+        String packageName = itemInfo.getTargetComponent().getPackageName();
+        return packageName == null ? null : new KillApp(activity, itemInfo, originalView);
+    };
+
+    public static class KillApp extends SystemShortcut<BaseDraggingActivity> {
+        private final String mPackageName;
+
+        public KillApp(BaseDraggingActivity target, ItemInfo itemInfo, View originalView) {
+            super(R.drawable.ic_kill_app, R.string.recent_task_option_kill_app,
+                    target, itemInfo, originalView);
+            mPackageName = itemInfo.getTargetComponent().getPackageName();
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mPackageName != null) {
+                IActivityManager iam = ActivityManagerNative.getDefault();
+                try {
+                    iam.forceStopPackage(mPackageName, UserHandle.USER_CURRENT);
+                    Toast appKilled = Toast.makeText(mTarget, R.string.recents_app_killed, Toast.LENGTH_SHORT);
+                    appKilled.show();
+                    AbstractFloatingView.closeAllOpenViews(mTarget);
+                } catch (RemoteException e) { }
+            }
         }
     }
 

BlissRoms/platform_packages_apps_Launcher3

Signed-off-by: Alvin Francis <nivlafx@gmail.com>
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: Dmitrii <bankersenator@gmail.com>
Signed-off-by: Jackeagle <jackeagle102@gmail.com>

src/com/android/launcher3/popup/SystemShortcut.java
@@ -15,6 +15,7 @@ import static com.android.launcher3.LauncherSettings.Favorites.ITEM_TYPE_TASK;
 
 import android.app.Activity;
 import android.app.ActivityManagerNative;
+import android.app.IActivityManager;
 import android.app.AlertDialog;
 import android.app.AppGlobals;
 import android.content.ComponentName;
@@ -565,6 +566,35 @@ public abstract class SystemShortcut<T extends ActivityContext> extends ItemInfo
             activityOptions.setSplashScreenStyle(SplashScreen.SPLASH_SCREEN_STYLE_ICON);
             activityOptions.setTaskOverlay(true /* taskOverlay */, true /* canResume */);
             return activityOptions;
+        }
+    }
+
+
+    public static final Factory<ActivityContext> KILL_APP = (activity, itemInfo, originalView) -> {
+        String packageName = itemInfo.getTargetComponent().getPackageName();
+        return packageName == null ? null : new KillApp(activity, itemInfo, originalView);
+    };
+
+    public static class KillApp extends SystemShortcut<ActivityContext> {
+        private final String mPackageName;
+
+        public KillApp(ActivityContext target, ItemInfo itemInfo, View originalView) {
+            super(R.drawable.ic_kill_app, R.string.recent_task_option_kill_app,
+                    target, itemInfo, originalView);
+            mPackageName = itemInfo.getTargetComponent().getPackageName();
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mPackageName != null) {
+                IActivityManager iam = ActivityManagerNative.getDefault();
+                try {
+                    iam.forceStopPackage(mPackageName, UserHandle.USER_CURRENT);
+                    Toast appKilled = Toast.makeText(view.getContext(), R.string.recents_app_killed, Toast.LENGTH_SHORT);
+                    appKilled.show();
+                    AbstractFloatingView.closeAllOpenViews(mTarget);
+                } catch (RemoteException e) { }
+            }
         }
     }
 