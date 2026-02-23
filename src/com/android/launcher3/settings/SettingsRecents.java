c16a19a584d95443836ea9935e30ada348265cc5
Pranav Vashi <neobuddy89@gmail.com>
2026-02-23 20:55:00 +0530

    Launcher3: Remove quick actions category on tablet
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- src/com/android/launcher3/settings/SettingsRecents.java
+++ src/com/android/launcher3/settings/SettingsRecents.java
@@ -19,11 +19,14 @@ package com.android.launcher3.settings;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
 
 import com.android.launcher3.LauncherFiles;
 import com.android.launcher3.LauncherPrefs;
 import com.android.launcher3.R;
+import com.android.launcher3.util.DisplayController;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
 import com.android.settingslib.widget.SettingsBasePreferenceFragment;
@@ -33,6 +36,8 @@ import com.android.settingslib.widget.SettingsBasePreferenceFragment;
  */
 public class SettingsRecents extends CollapsingToolbarBaseActivity
         implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private static final String RECENTS_CATEGORY_ACTION = "recents_category_actions";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,6 +75,32 @@ public class SettingsRecents extends CollapsingToolbarBaseActivity
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             getPreferenceManager().setSharedPreferencesName(LauncherFiles.SHARED_PREFERENCES_KEY);
             setPreferencesFromResource(R.xml.launcher_recents_preferences, rootKey);
+
+            PreferenceScreen screen = getPreferenceScreen();
+            for (int i = screen.getPreferenceCount() - 1; i >= 0; i--) {
+                Preference preference = screen.getPreference(i);
+                if (!initPreference(preference)) {
+                    screen.removePreference(preference);
+                }
+            }
+        }
+
+        /**
+         * Initializes a preference. This is called for every preference. Returning false here
+         * will remove that preference from the list.
+         */
+        protected boolean initPreference(Preference preference) {
+            String key = preference.getKey();
+            if (key == null) {
+                return true;
+            }
+
+            DisplayController.Info info = DisplayController.INSTANCE.get(getContext()).getInfo();
+            if (key.equals(RECENTS_CATEGORY_ACTION)) {
+                return !info.isTablet(info.realBounds);
+            }
+
+            return true;
         }
     }
 }
