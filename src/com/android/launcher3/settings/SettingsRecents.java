76f6438e528a983dba2fffb84ae7ecdb36f56efc
Jackeagle <jackeagle102@gmail.com>
2026-03-26 22:50:29 +0530

    Launcher3: Hide Lens toggle when Google Search is not installed
    No point showing it on non-GApps builds where it can't do anything.
    
    Change-Id: I61f5842d6d412580b701a7d1387690ec54283feb
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- src/com/android/launcher3/settings/SettingsRecents.java
+++ src/com/android/launcher3/settings/SettingsRecents.java
@@ -20,13 +20,14 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.preference.Preference;
-import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceGroup;
 import androidx.preference.PreferenceScreen;
 
 import com.android.launcher3.LauncherAppState;
 import com.android.launcher3.LauncherFiles;
 import com.android.launcher3.LauncherPrefs;
 import com.android.launcher3.R;
+import com.android.launcher3.Utilities;
 import com.android.launcher3.util.DisplayController;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
@@ -72,16 +73,32 @@ public class SettingsRecents extends CollapsingToolbarBaseActivity
      */
     public static class RecentsSettingsFragment extends SettingsBasePreferenceFragment {
 
+        private static final String KEY_RECENTS_LENS = "pref_recents_lens";
+
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             getPreferenceManager().setSharedPreferencesName(LauncherFiles.SHARED_PREFERENCES_KEY);
             setPreferencesFromResource(R.xml.launcher_recents_preferences, rootKey);
 
             PreferenceScreen screen = getPreferenceScreen();
-            for (int i = screen.getPreferenceCount() - 1; i >= 0; i--) {
-                Preference preference = screen.getPreference(i);
-                if (!initPreference(preference)) {
-                    screen.removePreference(preference);
+            filterPreferenceGroup(screen);
+        }
+
+        private void filterPreferenceGroup(PreferenceGroup group) {
+            for (int i = group.getPreferenceCount() - 1; i >= 0; i--) {
+                Preference preference = group.getPreference(i);
+                if (preference instanceof PreferenceGroup) {
+                    if (RECENTS_CATEGORY_ACTION.equals(preference.getKey())) {
+                        DisplayController.Info info =
+                                DisplayController.INSTANCE.get(getContext()).getInfo();
+                        if (info.isTablet(info.realBounds)) {
+                            group.removePreference(preference);
+                            continue;
+                        }
+                    }
+                    filterPreferenceGroup((PreferenceGroup) preference);
+                } else if (!initPreference(preference)) {
+                    group.removePreference(preference);
                 }
             }
         }
@@ -90,15 +107,16 @@ public class SettingsRecents extends CollapsingToolbarBaseActivity
          * Initializes a preference. This is called for every preference. Returning false here
          * will remove that preference from the list.
          */
-        protected boolean initPreference(Preference preference) {
+        private boolean initPreference(Preference preference) {
             String key = preference.getKey();
             if (key == null) {
                 return true;
             }
 
-            DisplayController.Info info = DisplayController.INSTANCE.get(getContext()).getInfo();
-            if (key.equals(RECENTS_CATEGORY_ACTION)) {
-                return !info.isTablet(info.realBounds);
+            if (key.equals(KEY_RECENTS_LENS) && !Utilities.isGSAEnabled(getContext())) {
+                LauncherPrefs.getPrefs(getContext()).edit()
+                        .putBoolean(KEY_RECENTS_LENS, false).apply();
+                return false;
             }
 
             return true;
